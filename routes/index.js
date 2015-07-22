58f4ef5eb954926c29f57a2cebe4acb5860dc058
Matthias Gattermeier <matthias.gattermeier@gmail.com>
2015-07-21 17:34:55 -0700

    socket io channel switching

--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,31 @@
 var path = require('path');
 
-module.exports = function(app){
-  
+module.exports = function(app) {
 
-  app.get('/', function(req, res){
+
+  app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../public/client/index.html'));
-  }); //app.get
+  });
 
-  app.get('/classes/messages', function(req, res){
+  app.get('/classes/messages', function(req, res) {
     var statusCode = 200;
     headers['Content-Type'] = "application/json";
     res.writeHead(statusCode, headers);
-    res.end(JSON.stringify(messages));  
+    res.end(JSON.stringify(messages));
   });
 
-  app.post('/classes/messages', function(req, res){
+  app.post('/classes/messages', function(req, res) {
     var statusCode = 201;
     headers['Content-Type'] = "application/json";
     res.writeHead(statusCode, headers);
 
     var body = ''; //reset body variable
-    
-    req.on('data', function(chunk){
+
+    req.on('data', function(chunk) {
       body += chunk;
     }); //req
-    
-    req.on('end', function(){
+
+    req.on('end', function() {
       messages.results.push(JSON.parse(body));
       res.end(JSON.stringify(messages));
     }); //req.on
--- server/basic-server.js
+++ server/basic-server.js
@@ -1,30 +1,37 @@
-/* Import node's http module: */
-
 var handleRequest = require("./request-handler.js")
 var express = require("express");
 var app = express();
-var http = require("http").Server(app); //??? 
+var http = require("http").Server(app);
 
 var io = require('socket.io')(http);
 var path = require("path");
 
-var routes = require("../routes"); //automatically loads index.js in /routes
+var routes = require("../routes");
 var port = 3000;
 
-// give us access (static, public) to the /client/ directory
-var publicPath = path.join(__dirname, '../public');
-console.log(publicPath);
-
-
-app.use('/public', express.static(publicPath));
+app.use('/public', express.static(path.join(__dirname, '../public')));
 routes(app);
 
-io.on('connection', function(socket){
-  console.log('client connected');
-  
-  socket.on('msg', function(msg){
-    console.log(msg);
-    io.emit('msg', msg);
+var defaults = {
+  chatrooms: ['Main', 'Second', 'Third']
+};
+
+io.on('connection', function(socket) {
+  console.log('client connected: ', socket.id);
+
+  // send socket default chatrooms
+  io.to(socket.id).emit('init', defaults);
+  // join main chatroom
+  socket.join(defaults.chatrooms[0]);
+
+  socket.on('chat message', function(data) {
+    io.to(data['chatroom']).emit('broadcast chat', data);
+  })
+
+  socket.on('channel change', function(data) {
+    console.log(socket.id, ' wants to join channel: ', data);
+    socket.leave();
+    socket.join(data);
   })
 
   socket.on('disconnect', function() {
@@ -33,7 +40,6 @@ io.on('connection', function(socket){
 })
 
 
-
-http.listen(port, function(){
-  console.log('listening on: ', port);  
-}); //http.listen
+http.listen(port, function() {
+  console.log('listening on: ', port);
+});
